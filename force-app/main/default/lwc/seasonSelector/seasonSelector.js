9a0aafadfa7916fe4314a0b6cfc95f84b229d80b
Marcin Skladanowski <marcin.skladanowski@accenture.com>
2022-08-02 18:10:11 +0200

    add nested accordions for each series

--- force-app/main/default/lwc/seasonSelector/seasonSelector.js
+++ force-app/main/default/lwc/seasonSelector/seasonSelector.js
@@ -1,13 +1,16 @@
 import { LightningElement, api, track } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import getSeries from '@salesforce/apex/DataCollector.getSeries';
-import getSeasons from '@salesforce/apex/DataCollector.getSeasons';
+// import getSeasons from '@salesforce/apex/DataCollector.getSeasons';
 import getEpisodes from '@salesforce/apex/DataCollector.getEpisodes';
 
 export default class LightningExampleAccordionBasic extends LightningElement {
 
     @track Series;
+    @track seasonsForSelectedSeries;
 
+    
+    _seriesCollapsed = false;
     _toastTitle;
     _toastMessage;
 
@@ -25,6 +28,25 @@ export default class LightningExampleAccordionBasic extends LightningElement {
         })
         this.isLoading = false; 
     }
+
+    // above ok
+
+    // askForSeasons(event) {
+    //     console.log('handleToggleSection works')
+    //     let id = event.currentTarget.dataset.id;
+    //     console.log('id assigned: ', id);
+    //     getSeasons({outSeries : id})
+    //     .then(result => {
+    //         this.seasonsForSelectedSeries = result;
+    //     })
+    //     .catch(error => {
+    //         this._toastTitle = 'Series:';
+    //         this._toastMessage = 'No seasons for this series available in database.';
+    //         this.showToast();
+    //     })
+    // }
+    
+    // below ok
 
     showToast() {
         const event = new ShowToastEvent({
